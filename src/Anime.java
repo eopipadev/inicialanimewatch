46fae301aebfa2a91247fd26680e00ade21e2a41
Ryan Alves <ryanvalves2002@gmail.com>
2025-06-15 13:30:54 -0300

    atualizando o codigo da classe principal

--- src/Anime.java
+++ src/Anime.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Anime {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double somaDasAvaliacoes;
+    int totalDeAvaliacoes;
+
+    void exibeFichaTecnica(){
+        System.out.println("Nome do Anime: " + nome);
+        System.out.println("Ano de Lancamento: " + anoDeLancamento);
+    }
+
+    void avalia(double nota){
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Principal {
+    public static void main(String[] args){
+        Anime anime1 = new Anime();
+
+        anime1.nome = "One Piece";
+        anime1.anoDeLancamento = 1999;
+        anime1.somaDasAvaliacoes = 5;
+        anime1.incluidoNoPlano = true;
+
+        anime1.exibeFichaTecnica();
+        anime1.avalia(5);
+        anime1.avalia(4.5);
+        anime1.avalia(4.8);
+
+        System.out.println("Media de avaliacoes do Anime: " +anime1.pegaMedia());
+    }
 }
